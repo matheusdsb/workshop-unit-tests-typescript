9873b7d424a38899cbe3b2bb115d6d55664146f3
Matheus Sabino <matheusdsb@gmail.com>
2022-01-13 00:06:07 -0300

    add user controller

--- src/default/controllers/users.controller.ts
+++ src/default/controllers/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { GitHubUserService } from '../services/github-user.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly githubUserService: GitHubUserService) {}
+
+  @Get('list-from-github')
+  async listFromGithub(
+    @Query('idGreaterThan') idGreaterThan: number,
+    @Query('pageSize') pageSize: number,
+  ) {
+    return await this.githubUserService.list(idGreaterThan, pageSize);
+  }
+
+  @Get('import-from-github')
+  async importFromGithub(
+    @Query('idGreaterThan') idGreaterThan: number,
+    @Query('onlyAdmins') onlyAdmins: string,
+    @Query('onlyWithAvatar') onlyWithAvatar: string,
+  ) {
+    return await this.githubUserService.import(
+      onlyAdmins === 'true' ? true : false,
+      onlyWithAvatar === 'true' ? true : false,
+      isNaN(idGreaterThan) ? 0 : Number(idGreaterThan),
+    );
+  }
+}
