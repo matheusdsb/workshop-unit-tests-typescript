26517ebe371d5f25e90d3308137bb26ffe4a8453
Matheus Sabino <matheusdsb@gmail.com>
2022-01-14 20:28:42 -0300

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DefaultModule } from './default/default.module';
 
 @Module({
-  imports: [DefaultModule],
+  imports: [
+    DefaultModule,
+    MongooseModule.forRoot('mongodb://localhost/workshopUnitTests'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
