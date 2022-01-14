d8d19ea48d847bb34ec8cde2a0db56126db96aa4
Matheus Sabino <matheusdsb@gmail.com>
2022-01-14 01:07:49 -0300

    add method to populate items

--- src/default/services/github-user.service.ts
+++ src/default/services/github-user.service.ts
@@ -1,5 +1,7 @@
-import { HttpService, Inject, Injectable } from '@nestjs/common';
+import { HttpService, Inject, Injectable, Logger } from '@nestjs/common';
+import { GithubUserFilter } from '../models/github-user-filter';
 import { User } from '../models/user';
+import { TimerHelper } from '../helpers/timer.helper';
 
 export type GithubUser = {
   login: string;
@@ -10,6 +12,7 @@ export type GithubUser = {
 
 @Injectable()
 export class GitHubUserService {
+  private logger = new Logger(GitHubUserService.name);
   // API REFERENCE https://docs.github.com/pt/rest/reference/users
   static MAX_PAGE_SIZE = 100;
 
@@ -31,25 +34,54 @@ export class GitHubUserService {
       );
       return result?.data ? this.toUsers(result.data) : [];
     } catch (error) {
+      this.logger.error(error.message, error.stack);
       throw new Error('Error consuming Github Users API');
     }
   }
 
-  async import(onlyAdmins = false, onlyWithAvatar = false, idGreaterThan = 0) {
-    const itemsFound = await this.list(idGreaterThan);
-    let itemsToImport = itemsFound.map((i) => i);
+  async listByFilters(filter: GithubUserFilter): Promise<User[]> {
+    const itemsFound = await this.list(filter.idGreaterThan);
+    return this.filterList(itemsFound, filter);
+  }
 
-    if (onlyAdmins) {
-      itemsToImport = itemsToImport.filter((i) => i.isSiteAdmin === true);
+  private filterList(items: User[], filter: GithubUserFilter): User[] {
+    let newList = items.map((i) => i);
+
+    if (filter.onlyAdmins) {
+      newList = newList.filter((i) => i.isSiteAdmin === true);
     }
 
-    if (onlyWithAvatar) {
-      itemsToImport = itemsToImport.filter(
-        (i) => i.avatar && i.avatar.length > 0,
+    if (filter.onlyWithAvatar) {
+      newList = newList.filter((i) => i.avatar && i.avatar.length > 0);
+    }
+
+    if (filter.loginStartsWith && filter.loginStartsWith.trim().length > 0) {
+      newList = newList.filter((i) =>
+        i.login
+          .toLowerCase()
+          .startsWith(filter.loginStartsWith.toLowerCase().trim()),
       );
     }
 
-    return itemsToImport;
+    return newList;
+  }
+
+  async populate(
+    filter: GithubUserFilter,
+    totalItems: number,
+  ): Promise<User[]> {
+    let lastFoundId = 0;
+    let currentPage = await this.list(lastFoundId + 1, 100);
+    const items: User[] = [];
+
+    while (items.length < totalItems && currentPage.length > 0) {
+      lastFoundId = currentPage[currentPage.length - 1].id;
+      items.push(...this.filterList(currentPage, filter));
+      await TimerHelper.sleep(500);
+      currentPage = await this.list(lastFoundId + 1, 100);
+    }
+
+    return items;
   }
 
   private toUsers(githubUsers: GithubUser[]): User[] {
