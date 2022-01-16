a6250bbab542830e507cdd788890a5ea81ac511c
Matheus Sabino <matheusdsb@gmail.com>
2022-01-16 17:36:12 -0300

    create approach to create or update user

--- src/default/services/user.service.ts
+++ src/default/services/user.service.ts
@@ -7,7 +7,17 @@ import { User, UserDocument } from '../models/user';
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async create(user: User): Promise<User> {
+  async createOrUpdate(user: User): Promise<User> {
+    const existingUser = await this.userModel
+      .find({ login: user.login })
+      .exec();
+    if (existingUser.length > 0) {
+      return await existingUser[0].update({
+        isSiteAdmin: user.isSiteAdmin,
+        avatar: user.avatar,
+      });
+    }
+
     const createdUser = new this.userModel(user);
     return await createdUser.save();
   }
