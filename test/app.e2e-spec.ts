443d2d1c66f44d634f7588f66f09212acdf2820f
Matheus Dias Sabino <matheus.dias.sabino@avanade.com>
2022-01-17 19:10:13 -0300

    fix app closing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,6 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  after(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
